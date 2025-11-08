745d3824235b502eafe82980b5288592a2c672b5
vixy-1 <morita79bd@gmail.com>
2025-11-08 19:47:17 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// ログインボタンを押したときの動作
+document.getElementById("loginBtn").addEventListener("click", () => {
+  alert("ログインページは準備中です！");
+});
+
+// 詳細ボタンを押したときの動作
+document.getElementById("infoBtn").addEventListener("click", () => {
+  alert("このサイトはVS Code + Live Serverで作られています！");
+});
